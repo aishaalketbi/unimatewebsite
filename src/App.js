e98a8c84382f6efe042a71bc95640cae717964b6
aishaalketbi <aa8680@nyu.edu>
2024-11-28 15:48:38 +0400

    Update App.js for correct routing in GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
+// import Home from './pages/Home';
+// import Dashboard from './pages/Dashboard';
+// import OnBoarding from './pages/OnBoarding';
+// import { BrowserRouter, Routes, Route} from 'react-router-dom'
+// import {useCookies} from "react-cookie";
+//
+// const App = () => {
+//     const [ cookies, setCookies, removeCookie] = useCookies(['user']);
+//
+//     const authToken = cookies.AuthToken
+//
+//     return (
+//         <BrowserRouter basename="/unimatewebsite">
+//             <Routes>
+//                 <Route path="/" element={<Home />} />
+//                 {authToken && <Route path="/dashboard" element={<Dashboard />} />}
+//                 {authToken && <Route path="/onboarding" element={<OnBoarding />} />}
+//             </Routes>
+//         </BrowserRouter>
+//     )
+// }
+//
+// export default App
+
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import OnBoarding from './pages/OnBoarding';
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import {useCookies} from "react-cookie";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 
 const App = () => {
-    const [ cookies, setCookies, removeCookie] = useCookies(['user']);
+    const [cookies, setCookies, removeCookie] = useCookies(['user']);
 
-    const authToken = cookies.AuthToken
+    const authToken = cookies.AuthToken;
 
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/unimatewebsite">
             <Routes>
-                <Route path="/home" element={<Home />} />
+                {/* Default Route */}
+                <Route path="/" element={<Home />} />
+
+                {/* Authenticated Routes */}
                 {authToken && <Route path="/dashboard" element={<Dashboard />} />}
                 {authToken && <Route path="/onboarding" element={<OnBoarding />} />}
+
+                {/* Optional: Catch-all route for undefined paths */}
+                <Route path="*" element={<Home />} />
             </Routes>
         </BrowserRouter>
-    )
-}
+    );
+};
 
-export default App
+export default App;
