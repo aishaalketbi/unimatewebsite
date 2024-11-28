1cc03b00b98e4ff230cf6f20ccff24b8dbb1229b
aishaalketbi <aa8680@nyu.edu>
2024-11-28 21:42:00 +0400

    changed to hashed

--- src/App.js
+++ src/App.js
@@ -22,10 +22,40 @@
 //
 // export default App
 
+// import Home from './pages/Home';
+// import Dashboard from './pages/Dashboard';
+// import OnBoarding from './pages/OnBoarding';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { useCookies } from 'react-cookie';
+//
+// const App = () => {
+//     const [cookies, setCookies, removeCookie] = useCookies(['user']);
+//
+//     const authToken = cookies.AuthToken;
+//
+//     return (
+//         <BrowserRouter basename="/unimatewebsite">
+//             <Routes>
+//                 {/* Default Route */}
+//                 <Route path="/" element={<Home />} />
+//
+//                 {/* Authenticated Routes */}
+//                 {authToken && <Route path="/dashboard" element={<Dashboard />} />}
+//                 {authToken && <Route path="/onboarding" element={<OnBoarding />} />}
+//
+//                 {/* Optional: Catch-all route for undefined paths */}
+//                 <Route path="*" element={<Home />} />
+//             </Routes>
+//         </BrowserRouter>
+//     );
+// };
+//
+// export default App;
+
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import OnBoarding from './pages/OnBoarding';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
 
 const App = () => {
@@ -34,7 +64,7 @@ const App = () => {
     const authToken = cookies.AuthToken;
 
     return (
-        <BrowserRouter basename="/unimatewebsite">
+        <HashRouter>
             <Routes>
                 {/* Default Route */}
                 <Route path="/" element={<Home />} />
@@ -46,7 +76,7 @@ const App = () => {
                 {/* Optional: Catch-all route for undefined paths */}
                 <Route path="*" element={<Home />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
